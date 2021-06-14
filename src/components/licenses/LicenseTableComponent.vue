fc86eec7764e2bfb3fb327725e95a0c40cfb7cca
Yariv.Gdidi <yariv.gdidi@incredibuild.com>
2021-06-14 10:39:46 +0300

    pagination

--- src/components/licenses/LicenseTableComponent.vue
+++ src/components/licenses/LicenseTableComponent.vue
@@ -1,13 +1,26 @@
 <template>
     <q-table
+      title="License table"
+      row-key="id"
+      filter= 'filter'
       class="license-table"
       :rows="getLicenses"
       :columns="columns"
-      row-key="id"
-      virtual-scroll
       v-model:pagination="pagination"
-      :rows-per-page-options="[0]"
-    />
+      @request="onRequest"
+      :loading="loading"
+      binary-state-sort
+    >
+      <template v-slot:top-right>
+        <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+
+    </q-table>
+
 
 </template>
 <script lang="ts">
@@ -23,15 +36,36 @@ const licenseModule = getModule(LicenseModule, store);
 import { License,  LicenseStatusEnum } from 'src/openApiClient/CloudAdminClient'
 import {Options, Vue} from 'vue-class-component';
 import moment from 'moment'
+import {Pagination} from 'src/model/pagination';
+import {Watch} from 'vue-property-decorator';
 @Options({})
 export default class LicenseTableComponent extends Vue {
-  columns: any[] = []
-  pagination: any = {};
+  columns: any[] = [];
+  loading = false;
+  filter: any = ref('');
+  pagination: any = ref({
+    sortBy: 'desc',
+    descending: false,
+    page: 1,
+    rowsPerPage: 3,
+    rowsNumber: 0
+  });
+
   @license.Getter
   public getLicenses!: License[];
 
-  getStatus(status: number): string {
+  @license.Getter
+  public getRowsNumber!: number;
+
+  @Watch('getRowsNumber')
+  getRowsNumberChanged(newVal: number) {
+    console.log('getRowsNumberChanged', newVal);
+    this.pagination.rowsNumber = newVal;
+  }
+
+  getStatus(status: any): string {
     const statuses: string[] = Object.keys(LicenseStatusEnum)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-member-access
     return statuses[status]
   }
 
@@ -39,18 +73,29 @@ export default class LicenseTableComponent extends Vue {
     return moment(date).format('MMM-D-YYYY');
   }
 
-  beforeCreate() {
-    void licenseModule.listLicenses();
+  onRequest(props: any): void {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
+    const { page, rowsPerPage = 0 , sortBy, descending } = props.pagination;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    this.pagination.rowsPerPage = rowsPerPage;
+    // this.loading = true;
+    const start = ( (page-1)  * rowsPerPage)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    void licenseModule.listLicenses(rowsPerPage, start);
+    // this.loading = false;
+  }
 
-    this.pagination = ref({
-      rowsPerPage: 0
-    })
+
+  created() {
+    const filter = ''
+    this.onRequest({pagination: this.pagination})
     this.columns = [
       { name: 'coordId',        label: 'Coordinator Id',  field: 'coordId',         sortable: true, align: 'left' },
       { name: 'status',         label: 'Status',          field: (val:any) => this.getStatus(val.status),          sortable: true, align: 'left'  },
-      { name: 'packageName',    label: 'Package Name',    field: (val:any) => val.package.name,     sortable: true, align: 'left' },
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-return
-      { name: 'packageType',    label: 'Package Type',    field: (val:any) => val.package.package_type,     sortable: true, align: 'left' },
+      { name: 'packageName',    label: 'Package Name',    field: (val:any) => val?.package?.name,     sortable: true, align: 'left' },
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-return
+      { name: 'packageType',    label: 'Package Type',    field: (val:any) => val?.package?.package_type,     sortable: true, align: 'left' },
       { name: 'activationDate', label: 'Activation Date', field: (val:any) => this.getFormattedDate(val.activation_date), sortable: true, align: 'left' },
       { name: 'email',          label: 'Email',           field: 'contact_person_email', sortable: true, align: 'left' },
       { name: 'comments',       label: 'Comments',        field: 'remarks',         sortable: true, align: 'left' },
---
+++ src/model/pagination.ts
@@ -0,0 +1,7 @@
+export interface Pagination {
+  sortBy: string,
+  descending: boolean,
+  page: number,
+  rowsPerPage: number,
+  rowsNumber: number
+};
